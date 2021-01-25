a5e1dc5913eb24e7314bd20290f938d92608ff6f
Vishesh Singh <VisheshSingh16@gmail.com>
2021-01-24 18:34:13 -0600

    wiring up pages🥀

--- src/pages/coaches/CoachDetails.vue
+++ src/pages/coaches/CoachDetails.vue
@@ -1,5 +1,7 @@
 <template>
   <h2>Coach Details</h2>
+  <router-link to="/coaches/c1/contact">Contact coach</router-link>
+  <router-view></router-view>
 </template>
 
 <script>
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -3,7 +3,10 @@
     FILTER
   </section>
   <section>
-    ALL COACHES
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register a Coach</router-link>
+    </div>
   </section>
 </template>
 
