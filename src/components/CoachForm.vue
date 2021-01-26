02e73943f458beb1a852c3f00a6603a5e73a297b
Vishesh Singh <VisheshSingh16@gmail.com>
2021-01-25 23:00:33 -0600

    Add form validation

--- src/components/CoachForm.vue
+++ src/components/CoachForm.vue
@@ -2,31 +2,77 @@
   <base-card>
     <h2>Coach Registration</h2>
     <form @submit.prevent="handleSubmit">
-      <div class="form-control">
+      <div class="form-control" :class="{ invalid: !firstname.isValid }">
         <label for="firstname">First Name</label>
-        <input type="text" id="firstname" v-model.trim="firstname" />
+        <input
+          type="text"
+          id="firstname"
+          v-model.trim="firstname.val"
+          @blur="clearValidity('firstname')"
+        />
+        <p v-if="!firstname.isValid">First Name cannot be blank</p>
       </div>
-      <div class="form-control">
+      <div class="form-control" :class="{ invalid: !lastname.isValid }">
         <label for="lastname">Last Name</label>
-        <input type="text" id="lastname" v-model.trim="lastname" />
+        <input
+          type="text"
+          id="lastname"
+          v-model.trim="lastname.val"
+          @blur="clearValidity('lastname')"
+        />
+        <p v-if="!lastname.isValid">Last Name cannot be blank</p>
       </div>
-      <div class="form-control">
+      <div class="form-control" :class="{ invalid: !description.isValid }">
         <label for="description">Description</label>
-        <textarea id="description" v-model.trim="description"></textarea>
+        <textarea
+          id="description"
+          v-model.trim="description.val"
+          @blur="clearValidity('description')"
+        ></textarea>
+        <p v-if="!description.isValid">Description cannot be blank</p>
       </div>
-      <div class="form-control">
+      <div class="form-control" :class="{ invalid: !rate.isValid }">
         <label for="rate">Hourly Rate</label>
-        <input type="number" id="rate" v-model.number="rate" />
+        <input
+          type="number"
+          id="rate"
+          v-model.number="rate.val"
+          @blur="clearValidity('rate')"
+        />
+        <p v-if="!rate.isValid">Please enter a valid hourly rate</p>
       </div>
       <h3>Area of expertise</h3>
-      <div class="form-control">
-        <input type="checkbox" id="frontend" value="frontend" v-model="areas" />
+      <p v-if="!areas.isValid">Please check at least one area of expertise</p>
+      <div class="form-control" :class="{ invalid: !areas.isValid }">
+        <input
+          type="checkbox"
+          id="frontend"
+          value="frontend"
+          v-model="areas.val"
+          @blur="clearValidity('areas')"
+        />
         <label for="frontend">Frontend</label>
-        <input type="checkbox" id="backend" value="backend" v-model="areas" />
+        <input
+          type="checkbox"
+          id="backend"
+          value="backend"
+          v-model="areas.val"
+          @blur="clearValidity('areas')"
+        />
         <label for="backend">Backend</label>
-        <input type="checkbox" id="career" value="career" v-model="areas" />
+
+        <input
+          type="checkbox"
+          id="career"
+          value="career"
+          v-model="areas.val"
+          @blur="clearValidity('areas')"
+        />
         <label for="career">Career</label>
       </div>
+      <p v-if="!formIsValid">
+        Please carefully fill all the missing details to complete registration
+      </p>
       <base-button>Register</base-button>
     </form>
   </base-card>
@@ -37,21 +83,70 @@ export default {
   emits: ['registration'],
   data() {
     return {
-      firstname: '',
-      lastname: '',
-      description: '',
-      rate: null,
-      areas: []
+      firstname: {
+        val: '',
+        isValid: true
+      },
+      lastname: {
+        val: '',
+        isValid: true
+      },
+      description: {
+        val: '',
+        isValid: true
+      },
+      rate: {
+        val: null,
+        isValid: true
+      },
+      areas: {
+        val: [],
+        isValid: true
+      },
+      formIsValid: true
     };
   },
   methods: {
+    checkValidity() {
+      this.formIsValid = true;
+
+      if (this.firstname.val === '') {
+        this.firstname.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.lastname.val === '') {
+        this.lastname.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.description.val === '') {
+        this.description.isValid = false;
+        this.formIsValid = false;
+      }
+      if (!this.rate.val || this.rate.val < 0) {
+        this.rate.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.areas.val.length === 0) {
+        this.areas.isValid = false;
+        this.formIsValid = false;
+      }
+    },
+    clearValidity(input) {
+      this[input].isValid = true;
+    },
     handleSubmit() {
+      this.checkValidity();
+
+      if (!this.formIsValid) {
+        return;
+      }
+
       const formData = {
-        firstName: this.firstname,
-        lastName: this.lastname,
-        description: this.description,
-        rate: this.rate,
-        areas: this.areas
+        firstName: this.firstname.val,
+        lastName: this.lastname.val,
+        description: this.description.val,
+        rate: this.rate.val,
+        areas: this.areas.val
       };
       this.$emit('registration', formData);
     }
