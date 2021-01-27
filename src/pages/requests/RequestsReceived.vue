bae4b6f393497eefe9454b1f600bfcf765630d2c
Vishesh Singh <VisheshSingh16@gmail.com>
2021-01-26 18:28:14 -0600

    Sending Coaching Requests Http Requests

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,32 +1,55 @@
 <template>
-  <section>
-    <base-card
-      ><header>
-        <h2>Requests Received</h2>
-      </header>
-      <ul v-if="hasRequests">
-        <request-item
-          v-for="req in requests"
-          :key="req.id"
-          :email="req.email"
-          :message="req.message"
-        ></request-item>
-      </ul>
-      <h3 v-else>You haven't received any requests yet!</h3>
-    </base-card>
-  </section>
+  <base-dialog :show="!!error" title="An error occured" @close="handleError">
+    <p>{{ error }}</p>
+  </base-dialog>
+  <base-card>
+    <header>
+      <h2>Requests Received</h2>
+    </header>
+    <base-spinner v-if="isLoading"></base-spinner>
+    <ul v-else-if="hasRequests && !isLoading">
+      <request-item
+        v-for="req in requests"
+        :key="req.id"
+        :email="req.email"
+        :message="req.message"
+      ></request-item>
+    </ul>
+    <h3 v-else>You haven't received any requests yet!</h3>
+  </base-card>
 </template>
 
 <script>
 import RequestItem from '../../components/RequestItem';
-import { mapGetters } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
   components: {
     RequestItem
   },
+  data() {
+    return {
+      isLoading: false,
+      error: null
+    };
+  },
   computed: {
     ...mapGetters('requests', ['requests', 'hasRequests'])
+  },
+  async created() {
+    this.isLoading = true;
+    try {
+      await this.fetchRequests();
+    } catch (err) {
+      this.error = err.message || 'Something went wrong!';
+    }
+    this.isLoading = false;
+  },
+  methods: {
+    ...mapActions('requests', ['fetchRequests']),
+    handleError() {
+      this.error = null;
+    }
   }
 };
 </script>
--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,11 +1,53 @@
 export default {
-  contactCoach({ commit }, payload) {
+  async contactCoach({ commit }, payload) {
     const newRequest = {
-      id: new Date().toISOString(),
-      coachId: payload.coachId,
       email: payload.email,
       message: payload.message
     };
+    const res = await fetch(
+      `https://vue-playlist-25566.firebaseio.com/requests/${payload.coachId}.json`,
+      {
+        method: 'POST',
+        body: JSON.stringify(newRequest)
+      }
+    );
+
+    const resData = await res.json();
+    console.log({ resData });
+    newRequest.id = resData.name;
+    newRequest.coachId = payload.coachId;
+
+    if (!res.ok) {
+      const error = new Error(resData.message || 'Failed to send request');
+      throw error;
+    }
     commit('ADD_REQUEST', newRequest);
+  },
+  async fetchRequests({ commit, rootGetters }) {
+    const coachId = rootGetters.getUserId;
+    const res = await fetch(
+      `https://vue-playlist-25566.firebaseio.com/requests/${coachId}.json`
+    );
+    const resData = await res.json();
+    console.log({ resData });
+
+    if (!res.ok) {
+      const error = new Error(resData.message || 'Failed to fetch requests');
+      throw error;
+    }
+
+    const requests = [];
+
+    for (let key in resData) {
+      const request = {
+        id: key,
+        coachId: coachId,
+        email: resData[key].email,
+        message: resData[key].message
+      };
+      requests.push(request);
+    }
+
+    commit('SET_REQUESTS', requests);
   }
 };
