37fa1cba18aa07af71b72edacd8f56bacb8cff5d
Vishesh Singh <VisheshSingh16@gmail.com>
2021-01-26 19:15:56 -0600

    not found comp

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,5 +1,13 @@
 <template>
-  <h2>Not found 💩</h2>
+  <section>
+    <base-card>
+      <h2>Page Not found 💩</h2>
+      <p>
+        This page is not found - may be look for our
+        <router-link to="/coaches">coaches</router-link>
+      </p>
+    </base-card>
+  </section>
 </template>
 
 <script>
