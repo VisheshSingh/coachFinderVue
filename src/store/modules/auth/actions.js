60d20860605e5d5d624bafe94a29b47e9e389b92
Vishesh Singh <VisheshSingh16@gmail.com>
2021-01-29 22:53:09 -0600

    Adding "Auto Login"

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,53 +1,42 @@
 export default {
-  async signup({ commit }, payload) {
-    const res = await fetch(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDxV8RPHEpVXo44K6jinM1Pe4cIayNj630`,
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true
-        })
-      }
-    );
-    const resData = await res.json();
-    console.log({ resData });
-
-    if (!res.ok) {
-      const error = new Error(
-        resData.error.message || 'Sign up failed. Please try again...'
-      );
-      throw error;
-    }
-
-    commit('SET_USER', {
-      token: resData.idToken,
-      userId: resData.localId,
-      expiresIn: resData.expiresIn
+  async signup({ dispatch }, payload) {
+    return dispatch('auth', {
+      ...payload,
+      mode: 'signup'
     });
   },
-  async login({ commit }, payload) {
-    const res = await fetch(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDxV8RPHEpVXo44K6jinM1Pe4cIayNj630`,
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true
-        })
-      }
-    );
+  async login({ dispatch }, payload) {
+    return dispatch('auth', {
+      ...payload,
+      mode: 'login'
+    });
+  },
+  async auth({ commit }, payload) {
+    const mode = payload.mode;
+    let url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDxV8RPHEpVXo44K6jinM1Pe4cIayNj630`;
+    if (mode === 'signup') {
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDxV8RPHEpVXo44K6jinM1Pe4cIayNj630`;
+    }
+    const res = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true
+      })
+    });
     const resData = await res.json();
     console.log({ resData });
 
     if (!res.ok) {
       const error = new Error(
-        resData.error.message || 'Login failed. Please try again...'
+        resData.error.message || 'Something went wrong. Please try again...'
       );
       throw error;
     }
+
+    localStorage.setItem('token', resData.idToken);
+    localStorage.setItem('userId', resData.localId);
 
     commit('SET_USER', {
       token: resData.idToken,
@@ -61,5 +50,17 @@ export default {
       userId: null,
       expiresIn: null
     });
+  },
+  tryLogin({ commit }) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+
+    if (token && userId) {
+      commit('SET_USER', {
+        token,
+        userId,
+        expiresIn: null
+      });
+    }
   }
 };
